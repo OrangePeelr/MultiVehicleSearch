d5351ae61a701d02740b27ebc57193b0b546de41
Orion <obr24@byu.edu>
2025-11-03 15:20:50 -0700

    Update main.py to return list as response

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from typing import Union
 from pydantic import BaseModel
 
 from fastapi import FastAPI
+
+from findListings import findListings
 
 app = FastAPI()
 
 class VehicleQuery(BaseModel):
     length: int
     quantity: int
+    
+class LocationsResponse(BaseModel):
+    locations: list
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def parse_vehicle_queries(vehicle_queries: list[VehicleQuery]) -> list[dict[str, int]]:
+    parsed = []
+    for query in vehicle_queries:
+        parsed.append({"length" : query.length, "quantity" : query.quantity})
+    return parsed
 
 @app.post("/")
-def get_items(vehicle_queries: list[VehicleQuery]) -> VehicleQuery:
-    return vehicle_queries[0]
+def get_items(vehicle_queries: list[VehicleQuery]) -> list[dict[str, str | list[str] | int]]:
+    print("this is the query\n\n\n\n\n")
+    print(vehicle_queries)
+    parsed_vehicle_queries = parse_vehicle_queries(vehicle_queries)
+    print(parsed_vehicle_queries)
+    response = findListings(parsed_vehicle_queries)
+    return response
